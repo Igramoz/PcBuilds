fbd942fc434ebf5a6bca60a8e15a59acee284042
Marco Magri <81182912+Igramoz@users.noreply.github.com>
2024-05-13 19:00:06 +0200

    Create index.js

---
+++ Api/routes/index.js
@@ -0,0 +1,36 @@
+//import builds from './builds.js';
+const chipsets = require('./chipsets.js');
+const psu = require('./psu.js');
+const cpus = require('./cpus.js');
+const gpus = require('./gpus.js');
+const storage = require('./storage.js');
+const users = require('./users.js');
+const user_builds = require('./user_builds.js');
+const QVL = require('./QVL.js');
+const builds = require('./builds.js');
+const cases = require('./case.js');
+const storage_lengths = require('./storage_lengths.js');
+const types_ram = require('./types_ram.js');
+const cpu_coolers = require('./cpu_coolers.js');
+const ram = require('./ram.js');
+const motherboards = require('./motherboard.js');
+
+module.exports = {
+    builds,
+    chipsets,
+    cpus,
+    psu,
+    gpus,
+    storage,
+    users,
+    user_builds,
+    QVL,
+    cases,
+    storage_lengths,
+    types_ram,
+    cpu_coolers,
+    ram,
+    motherboards,
+    
+    
+}
